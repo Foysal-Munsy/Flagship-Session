9732695900862d29b796086d6205fa5ff90ddc45
Foysal-Munsy <bdfoysal555@gmail.com>
2025-04-25 06:57:35 +0600

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Router } from "react-router";
 import "./App.css";
+import { AArrowDownIcon } from "lucide-react";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! <AArrowDownIcon />
+      </h1>
       <button className="btn btn-primary">Btn</button>
       <button className="btn btn-outline btn-primary">Primary</button>
     </>
