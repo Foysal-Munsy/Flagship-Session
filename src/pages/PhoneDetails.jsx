592d6d2696b122638775a6059a99232514fb1852
Foysal-Munsy <bdfoysal555@gmail.com>
2025-04-29 22:50:39 +0600

    details added

--- src/pages/PhoneDetails.jsx
+++ src/pages/PhoneDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
 import Button from "../ui/Button";
+import { MdAddShoppingCart, MdBookmarkAdd } from "react-icons/md";
 
 const PhoneDetails = () => {
   const data = useLoaderData();
@@ -19,7 +20,57 @@ const PhoneDetails = () => {
     storage,
     camera_info,
   } = singlePhone || {};
-  return <div></div>;
+  return (
+    <div className="w-full mt-20">
+      <img src={image} className="w-full mx-auto md:w-auto  mb-8" alt="" />
+      <div className="flex justify-between">
+        <h1 className="text-6xl font-thin mb-8">{name}</h1>
+        <div className="space-x-2">
+          <Button label={<MdAddShoppingCart />}></Button>
+          <Button label={<MdBookmarkAdd />}></Button>
+        </div>
+      </div>
+      <div className="space-y-4">
+        <h2 className="font-thin text-4xl">Details: </h2>
+        <p>
+          <span className="font-bold">Brand:</span> {brand}
+        </p>
+        <p>
+          <span className="font-bold">Model:</span> {model}
+        </p>
+        {/* Storage Info */}
+        <div className="flex gap-1">
+          <p className="font-bold">Storage:</p>
+          <div>
+            {storage.map((item) => {
+              return (
+                <div key={item}>
+                  <div>{item}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* Price info */}
+        <div className="flex gap-1">
+          <p className="font-bold">Price:</p>
+          <div>
+            {Object.entries(price).map(([storage, price]) => (
+              <p key={storage} className="">
+                <span>{storage}:</span> <span>{price}</span>
+              </p>
+            ))}
+          </div>
+        </div>
+        <p>
+          <span className="font-bold">Camera Info:</span> {camera_info}
+        </p>
+        <p>
+          <span className="font-bold">Description:</span> {description}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PhoneDetails;
