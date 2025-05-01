9800090d3cd6a260bd353889f73d635d05a40009
Foysal-Munsy <bdfoysal555@gmail.com>
2025-05-01 21:51:28 +0600

    empty state

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getFavorites, removeFavorite } from "../utils";
 import PhoneCard from "../components/PhoneCard";
+import EmptyState from "../ui/EmptyState";
 
 const Favourites = () => {
   const [displayPhones, setDisplayPhones] = useState([]);
@@ -13,6 +14,7 @@ const Favourites = () => {
     removeFavorite(id);
     setDisplayPhones(getFavorites());
   };
+  if (displayPhones.length === 0) return <EmptyState></EmptyState>;
   return (
     <div className="py-12">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
