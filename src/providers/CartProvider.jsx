bb0ab0cd5b68d17919aefa8ccec49b20e17ec960
Foysal-Munsy <bdfoysal555@gmail.com>
2025-05-01 22:03:53 +0600

    context parent

---
+++ src/providers/CartProvider.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CartContext } from "./Contexts";
+
+const CartProvider = () => {
+  return <CartContext.Provider></CartContext.Provider>;
+};
+
+export default CartProvider;
